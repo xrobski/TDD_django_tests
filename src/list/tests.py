a95e51de25742cabcabf5d43e2f471d56d2e55e6
Robski <chrzmichal@gmail.com>
2019-07-22 15:32:01 +0200

    few steps ahead

---
+++ src/list/tests.py
@@ -0,0 +1,22 @@
+from django.urls import resolve
+from django.test import TestCase
+from django.http import HttpRequest
+from django.template.loader import render_to_string
+
+from list.views import home_page
+
+
+class  HomePageTest(TestCase):
+
+    def test_home_page_retutns_correct_html(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
--- src/tests/funcional_tests.py
+++ src/tests/funcional_tests.py
@@ -1,6 +1,79 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(
+            row_text,
+            [row.text for row in rows])
+
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Dongster has herd about a cool online to-do app.
+        # He goes to check its homepage
+        self.browser.get('http://localhost:8000')
+
+        # He notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # He is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # She types 'Buy peacock feathers' into a text box (Edith's hobbys
+        # is typing fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
+
+        # when he hits enter, the page updates and now the page lists
+        # '1: Buy peacock feathers' as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(
+            '1: Buy peacock feathers',
+            [row.text for row in rows]
+        )
+
+        # There is still a text box inviting her to add another item.
+        # She enters 'Use peacock feathers to make a fly'
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
+        )
+
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
